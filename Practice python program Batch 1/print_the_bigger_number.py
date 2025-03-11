a5922a16e3069468cf0fc1362476435ace1419c5
Kyiewi <kyrie.norriega@gmail.com>
2025-03-11 22:18:11 +0800

    Fixed spacing in conditional statements for bigger number program

--- Practice python program Batch 1/print_the_bigger_number.py
+++ Practice python program Batch 1/print_the_bigger_number.py
@@ -4,7 +4,7 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 #Check which number is bigger
-if num1>num2:
+if num1 > num2:
     #Print bigger number
     print ("The bigger number is:", num1)
 else:
