50b96d6d3769f1fcc154f4952b61caa2e2a14778
Kyiewi <kyrie.norriega@gmail.com>
2025-03-11 21:44:21 +0800

    print output for bigger number program

--- Practice python program Batch 1/print_the_bigger_number.py
+++ Practice python program Batch 1/print_the_bigger_number.py
@@ -4,4 +4,9 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 #Check which number is bigger
+if num1>num2:
     #Print bigger number
+    print (num1)
+
+else:
+    print (num2)
