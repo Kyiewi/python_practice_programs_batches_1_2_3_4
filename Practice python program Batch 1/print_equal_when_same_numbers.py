177c6ce43288e7f20ff57821ab11190999ce119e
Kyiewi <kyrie.norriega@gmail.com>
2025-03-11 22:34:57 +0800

    Fixed spacing and indentation in condition check for equal program

--- Practice python program Batch 1/print_equal_when_same_numbers.py
+++ Practice python program Batch 1/print_equal_when_same_numbers.py
@@ -1,7 +1,9 @@
 #Ask user to input 2 numbers
+
 num3 = float(input("Enter first number: "))
-num4 = float (input("Enter second number: "))
+num4 = float(input("Enter second number: "))
+
 #Check if equal numbers
-if num3==num4:
- #Print if true
- print("Equal")
+if num3 == num4:
+    #Print if true
+    print("Equal")
