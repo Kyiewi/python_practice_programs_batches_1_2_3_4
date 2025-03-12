e7ad77c2cc2d7107681ccd5c968e618dd84e25be
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 21:24:39 +0800

    Program that asks the user for two numbers and prints the numbers between them

---
+++ Practice python program Batch 2/print_all_num_between_2_nums.py
@@ -0,0 +1,7 @@
+#Ask user to input two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+#Check and print the numbers between the two inputted numbers
+for numbers in range(num1 + 1, num2):
+    print(number)
