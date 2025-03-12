009cbecdf945efac3e741eb46af6d8b1c5f19759
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 21:06:49 +0800

    Program that prints all odd numbers from 0 to 100

---
+++ Practice python program Batch 2/print_odd_nums_from_0_to_100.py
@@ -0,0 +1,10 @@
+# Set  initial number to 1 
+number = 1
+
+# while loop until greater than 100
+while number <= 100:
+    # Print odd number
+    print(number)  
+    
+    # Add 2 to the number to get the next odd number
+    number += 2
