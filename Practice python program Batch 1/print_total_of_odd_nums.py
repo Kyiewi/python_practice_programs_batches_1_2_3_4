43be7b841868728d75d5babf07b1475eb6706d67
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-12 20:28:34 +0800

    Update print_total_of_odd_nums.py

--- Practice python program Batch 1/print_total_of_odd_nums.py
+++ Practice python program Batch 1/print_total_of_odd_nums.py
@@ -1,8 +1,11 @@
 # Get 10 numbers from the user
-numbers = [int(input("Enter a number: ")) for num in range(10)]
-
+nums = []
+for num in range(10):
+    number = int(input("Enter a number: "))
+    nums.append(number)
+  
 # Count the odd numbers in the list
-odd_count = sum(1 for num in numbers if num % 2 != 0)
+odd_count = sum(1 for num in nums if num % 2 != 0)
 
 # Print the total count of odd numbers
 print("The count of odd numbers is:", odd_count)
