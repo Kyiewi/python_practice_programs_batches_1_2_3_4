3c4e453db54bdf4a29afe4b3ea75a3f7cdfadc53
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 20:24:26 +0800

    program to ask user for 10 numbers and print the total of even numbers

---
+++ Practice python program Batch 2/print_total_of_even_nums.py
@@ -0,0 +1,11 @@
+# Get 10 numbers from user
+nums = []
+for num in range(10):
+    number = float(input("Enter a number: "))
+    nums.append(number)
+    
+# Count the even numbers in the list
+even_count = sum(1 for num in nums if num % 2 == 0)
+
+# Print the total count of even numbers
+print("The count of even numbers is:", even_count)
