c5e11dc7438b2d2bbf9e54f72a5fe84d3ce25b41
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 17:38:56 +0800

    Add pseudocode to get 10 inputs, count odd numbers, and print total count of odd numbers for total odd num program

---
+++ Practice python program Batch 1/print_total_of_odd_nums.py
@@ -0,0 +1,9 @@
+# Get 10 numbers from the user
+numbers = [int(input("Enter a number: ")) for _ in range(10)]
+
+# Count the odd numbers in the list
+odd_count = sum(1 for num in numbers if num % 2 != 0)
+
+# Print the total count of odd numbers
+print("The count of odd numbers is:", odd_count)
+
