9df9cf10f00e3cde79ab4ef2d150b8e2261f7e16
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 19:18:57 +0800

    ask user for two numbers, divide the 2 numbers, and print the quotient without decimals

---
+++ Practice python program Batch 2/print_quotient_without_decimal.py
@@ -0,0 +1,6 @@
+#Ask user to input 2 numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+#Divide number and print the quotiend without decimal
+print (int(num1//num2))
