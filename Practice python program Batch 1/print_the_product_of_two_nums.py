a843b33be3d7c383212d7f2ff210398fa6933db1
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 15:16:57 +0800

    print the product for product of two numbers program

--- Practice python program Batch 1/print_the_product_of_two_nums.py
+++ Practice python program Batch 1/print_the_product_of_two_nums.py
@@ -1,5 +1,9 @@
 #Ask user to input 2 numbers
+
 num7 = float(input("Enter first number: "))
 num8 = float(input("Enter second number: "))
+
 #Multiply two numbers
+product = num7 * num8
 #Print the product
+print("The product of two numbers is:", product)
