ed970688113b499e8e2750cd8629e30859bb73cd
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 15:06:35 +0800

    Removed unnecessary parenthesis for sum of two numbers program

--- Practice python program Batch 1/print_the_sum_of_two_nums.py
+++ Practice python program Batch 1/print_the_sum_of_two_nums.py
@@ -3,6 +3,6 @@ num5 = float(input("Enter first number: "))
 num6 = float(input("Enter second number: "))
 
 #Add two numbers
-sum = (num5 + num6)
+sum = num5 + num6
 #print the sum
 print("The sum of two numbers is:", sum)
