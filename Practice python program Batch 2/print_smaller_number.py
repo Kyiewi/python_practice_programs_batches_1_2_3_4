37b25853e097fe57d68a7aee85e09328943fbacb
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 18:52:42 +0800

    ask user for two numbers, check for smaller number, and print smaller number

---
+++ Practice python program Batch 2/print_smaller_number.py
@@ -0,0 +1,10 @@
+#Ask user to input 2 numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+#Check which number is smaller
+if num1 < num2:
+    #print smaller number
+    print("The smaller number is:", num1)
+else:
+    print("The smaller number is:", num2)
