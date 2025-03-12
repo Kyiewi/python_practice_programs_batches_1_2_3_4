a9bcfe80c35d8608b19af18ed79796fddbed66b5
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 19:49:18 +0800

    Program to ask user for two numbers, divide, and print remainder

---
+++ Practice python program Batch 2/print_remainder_when_1stnum_divided_by_2ndnum.py
@@ -0,0 +1,6 @@
+# Ask user to input 2 numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Print remainder when num1 is divided by num2
+print("The remainder is:", num1 % num2)
