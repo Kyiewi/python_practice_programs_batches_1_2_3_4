7e630c51a85c776d715e83118ac4a9ab068f7522
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 16:00:51 +0800

    print the result for the raised num program

--- Practice python program Batch 1/print_result_1stnum_raised_to_2ndnum.py
+++ Practice python program Batch 1/print_result_1stnum_raised_to_2ndnum.py
@@ -1,5 +1,9 @@
 #Ask user to input 2 numbers
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
 #Raised the first num to second num
+result = (num1 **  num2)
+
 #Print output
+print("The result of", num1,"raised to the power of", num2,"is:", result)
