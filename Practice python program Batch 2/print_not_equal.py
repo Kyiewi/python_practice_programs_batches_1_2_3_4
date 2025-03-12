9f4ab9d4cf0b0885961ba84e1c88561b09c6d6e7
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 18:59:36 +0800

    ask user for two numbers, check if numbers are not equal then print not equal

---
+++ Practice python program Batch 2/print_not_equal.py
@@ -0,0 +1,8 @@
+#Ask user to input 2 numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+#Check if number is not equal
+if num1!=num2:
+    #print not equal
+    print("Not Equal")
