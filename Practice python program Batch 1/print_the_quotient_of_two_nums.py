faaa72542ed806ca4f782ec12d3acb2a21338177
Kyiewi <kyrie.norriega@gmail.com>
2025-03-12 15:32:41 +0800

    Ask user to input 2 num for quotient of two nums program

--- Practice python program Batch 1/print_the_quotient_of_two_nums.py
+++ Practice python program Batch 1/print_the_quotient_of_two_nums.py
@@ -1,2 +1,4 @@
-#Ask user to input 2 numbers 
+#Ask user to input 2 numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 #Divide two numbers and print quotient
