be22902b0792d89abb2061f72716bed3a32c0e82
Kyrie Norriega <kyrie.norriega@gmail.com>
2025-03-22 17:57:58 +0800

    Update print_all_num_between_2_nums.py

--- practice_python_program_batch_2/print_all_num_between_2_nums.py
+++ practice_python_program_batch_2/print_all_num_between_2_nums.py
@@ -3,5 +3,10 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 #Check and print the numbers between the two inputted numbers
-for numbers in range(num1 + 1, num2):
+if num1 < num2:
+for numbers in range(num1 + 1, num2)
+    print(number)
+else:
+    num1 > num2:
+    for numbers in range(num2 + 1, num1)
     print(number)
